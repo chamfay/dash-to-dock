chamfay/dash-to-dock

commit 702fd31d5978131d7c65d9b2f0c8b8308527322d

Force recalculation of the iconsise on theme changes.

dockedDash.js
@@ -1588,6 +1588,7 @@ const themeManager = new Lang.Class({
         this._getBackgroundColor();
         this._updateBackgroundOpacity();
         this._adjustTheme();
+        this._dash._redisplay();
     },
 
     /* Reimported back and adapted from atomdock */

commit 1f61cea8dc4643e565c4b7e6c97864ce50c7e73e

Fix wrong icon size on extension reload.
Setting the icon size before setting the theme avoid a wrong icon size on
extension reload (alt+F2 + r). This also improves the animation startup
avoiding the ugly slide-out while scaling animation.

The bug was introduced by 702fd31d5978131d7c65d9b2f0c8b8308527322d.

dockedDash.js
@@ -467,11 +467,10 @@ const dockedDash = new Lang.Class({
             this._paintId=0;
         }
 
+        this.dash.setIconSize(this._settings.get_int('dash-max-icon-size'));
 
         // Apply custome css class according to the settings
         this._themeManager.updateCustomTheme();
-
-        this.dash.setIconSize(this._settings.get_int('dash-max-icon-size'));
 
         // Since Gnome 3.8 dragging an app without having opened the overview before cause the attemp to
         //animate a null target since some variables are not initialized when the viewSelector is created